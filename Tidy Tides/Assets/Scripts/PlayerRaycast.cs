8dd68a6b903c10eaf6abbdd380a194c54ca54c85
Hayden <whitefiregames1@hotmail.com>
2021-06-30 12:14:47 +1000

    Beginning work on player interact

---
+++ Tidy Tides/Assets/Scripts/PlayerRaycast.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaycast : MonoBehaviour
+{
+    public float interactDistance;
+
+    private void Update()
+    {
+        
+    }
+}
